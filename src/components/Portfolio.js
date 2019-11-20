f9e6f6d383a556e20b3863aa3792493c4d2f3a0a
Vamsi Krishna <vamsi.krishna@trader.ca>
2019-11-19 23:53:06 -0800

    Creating portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    render() {
+
+        if(this.props.data) {
+            var projects = this.props.data.projects.map(function(projects){
+                
+            })
+        }
+
+        return(
+            <section id="portfolio">
+                
+            </section>
+        );
+    }
+
+    
+}
